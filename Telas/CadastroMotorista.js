3dd10a307b0bf74d03a031b91a36557c9c05c3fc
unknown <23070012@acad.cesupa>
2025-04-03 10:34:26 -0300

    Adicionando correções

--- Telas/CadastroMotorista.js
+++ Telas/CadastroMotorista.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
+
+const altura = Dimensions.get('window').height
 
 export default function CadastroMotorista({ navigation }) {
+
   const [exibirCadastro, setExibirCadastro] = useState(false);
 
   const [nome, setNome] = useState("");
@@ -9,7 +12,6 @@ export default function CadastroMotorista({ navigation }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [confirmacaoSenha, setConfirmacaoSenha] = useState("");
-
   const [nacionalidade, setNacionalidade] = useState("");
   const [cnhCategoria, setCnhCategoria] = useState("");
   const [cnhEmissao, setCnhEmissao] = useState("");
@@ -20,46 +22,39 @@ export default function CadastroMotorista({ navigation }) {
   const [emailLogin, setEmailLogin] = useState("");
   const [senhaLogin, setSenhaLogin] = useState("");
 
-  const [erroNome, setErroNome] = useState("");
-  const [erroTelefone, setErroTelefone] = useState("");
-  const [erroEmail, setErroEmail] = useState("");
-  const [erroSenha, setErroSenha] = useState("");
-  const [erroConfirmacaoSenha, setErroConfirmacaoSenha] = useState("");
+  const [erroNome, setErroNome] = useState("\n");
+  const [erroTelefone, setErroTelefone] = useState("\n");
+  const [erroEmail, setErroEmail] = useState("\n");
+  const [erroSenha, setErroSenha] = useState("\n");
+  const [erroConfirmacaoSenha, setErroConfirmacaoSenha] = useState("\n");
 
-  const [erroNacionalidade, setErroNacionalidade] = useState("");
-  const [erroCnhCategoria, setErroCnhCategoria] = useState("");
-  const [erroCnhEmissao, setErroCnhEmissao] = useState("");
-  const [erroCnhValidade, setErroCnhValidade] = useState("");
-  const [erroRg, setErroRg] = useState("");
-  const [erroCpf, setErroCpf] = useState("");
+  const [erroNacionalidade, setErroNacionalidade] = useState("\n");
+  const [erroCnhCategoria, setErroCnhCategoria] = useState("\n");
+  const [erroCnhEmissao, setErroCnhEmissao] = useState("\n");
+  const [erroCnhValidade, setErroCnhValidade] = useState("\n");
+  const [erroRg, setErroRg] = useState("\n");
+  const [erroCpf, setErroCpf] = useState("\n");
 
-  const [erroEmailLogin, setErroEmailLogin] = useState("");
-  const [erroSenhaLogin, setErroSenhaLogin] = useState("");
+  const [erroEmailLogin, setErroEmailLogin] = useState("\n");
+  const [erroSenhaLogin, setErroSenhaLogin] = useState("\n");
 
-  function CategoriaCnh(texto) {
-    
-    if (texto.length <= 2) {
-      setCnhCategoria(texto);
-    }
-  }
-  
   function continuarCadastro() {
       let informacoesValidas = true;
-  
+
       if (nome.trim() === "") {
           setErroNome("Campo obrigatório!");
           informacoesValidas = false;
       } else {
           setErroNome("");
       }
-  
+
       if (telefone.trim() === "") {
           setErroTelefone("Campo obrigatório!");
           informacoesValidas = false;
       } else {
           setErroTelefone("");
       }
-  
+
       if (email.trim() === "") {
           setErroEmail("Campo obrigatório!");
           informacoesValidas = false;
@@ -71,14 +66,14 @@ export default function CadastroMotorista({ navigation }) {
               informacoesValidas = false;
           }
       }
-  
+
       if (senha.trim() === "") {
           setErroSenha("Campo obrigatório!");
           informacoesValidas = false;
       } else {
           setErroSenha("");
       }
-  
+
       if (confirmacaoSenha.trim() === "") {
           setErroConfirmacaoSenha("Campo obrigatório!");
           informacoesValidas = false;
@@ -90,42 +85,42 @@ export default function CadastroMotorista({ navigation }) {
               setErroConfirmacaoSenha("");
           }
       }
-  
+
       if (nacionalidade.trim() === "") {
           setErroNacionalidade("Campo obrigatório!");
           informacoesValidas = false;
       } else {
           setErroNacionalidade("");
       }
-  
+
       if (cnhCategoria.trim() === "") {
           setErroCnhCategoria("Campo obrigatório!");
           informacoesValidas = false;
       } else {
           setErroCnhCategoria("");
       }
-  
+
       if (cnhEmissao.trim() === "") {
           setErroCnhEmissao("Campo obrigatório!");
           informacoesValidas = false;
       } else {
           setErroCnhEmissao("");
       }
-  
+
       if (cnhValidade.trim() === "") {
           setErroCnhValidade("Campo obrigatório!");
           informacoesValidas = false;
       } else {
           setErroCnhValidade("");
       }
-  
+
       if (rg.trim() === "") {
           setErroRg("Campo obrigatório!");
           informacoesValidas = false;
       } else {
           setErroRg("");
       }
-  
+
       if (cpf.trim() === "") {
           setErroCpf("Campo obrigatório!");
           informacoesValidas = false;
@@ -135,11 +130,11 @@ export default function CadastroMotorista({ navigation }) {
       if (!informacoesValidas) {
           return;
       }
-  
+
       setExibirCadastro(false);
       navigation.navigate("CadastrarVeiculo");
   }
-  
+
 
 function login() {
   let informacoesValidas = true;
@@ -169,11 +164,11 @@ function login() {
 }
 
 function NumeroRegistro(texto) {
-  let formatacao = texto; 
+  let formatacao = texto;
   if (texto.length > 11) {
-    formatacao = texto.substring(0, 11); 
+    formatacao = texto.substring(0, 11);
   }
-  setNumeroRegistro(formatacao); 
+  setNumeroRegistro(formatacao);
 }
 
 function CategoriaCnh(texto) {
@@ -186,88 +181,95 @@ function CategoriaCnh(texto) {
 }
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <ScrollView style={{flex: 1}} contentContainerStyle={styles.container}>
       {exibirCadastro ? (
         <>
           <Text style={styles.titulo}>Cadastro de Motorista</Text>
 
-          <TextInput 
-          placeholder="Nome"
-           style={styles.input} 
-           onChangeText={setNome} 
-           />
+          <TextInput
+           placeholder="Nome"
+           style={styles.input}
+           onChangeText={setNome}
+           value={nome}/>
           <Text style={styles.erro}>{erroNome}</Text>
 
-          <TextInput 
-          placeholder="Telefone"
-           keyboardType="phone-pad" 
-           style={styles.input} 
-           onChangeText={setTelefone} 
-           />
+          <TextInput
+           placeholder="Telefone"
+           keyboardType="phone-pad"
+           style={styles.input}
+           onChangeText={setTelefone}
+           value={telefone}/>
           <Text style={styles.erro}>{erroTelefone}</Text>
 
           <TextInput
            placeholder="Email"
-            keyboardType="email-address"
-             style={styles.input} 
-             onChangeText={setEmail} 
-             />
+           keyboardType="email-address"
+           style={styles.input}
+           onChangeText={setEmail}
+           value={email}/>
           <Text style={styles.erro}>{erroEmail}</Text>
 
-          <TextInput 
-          placeholder="Senha"
-           secureTextEntry style={styles.input} 
-           onChangeText={setSenha} 
-           />
+          <TextInput
+           placeholder="Senha" 
+           secureTextEntry
+           style={styles.input}
+           onChangeText={setSenha}
+           value={senha}/>
           <Text style={styles.erro}>{erroSenha}</Text>
 
-          <TextInput 
-          placeholder="Confirmar Senha"
-           secureTextEntry style={styles.input} 
-           onChangeText={setConfirmacaoSenha} 
-           />
+          <TextInput
+           placeholder="Confirmar Senha"
+           secureTextEntry
+           style={styles.input}
+           onChangeText={setConfirmacaoSenha}
+           value={confirmacaoSenha}/>
           <Text style={styles.erro}>{erroConfirmacaoSenha}</Text>
 
           <TextInput
            placeholder="Nacionalidade"
            style={styles.input}
-           onChangeText={setNacionalidade} 
-           />
+           onChangeText={setNacionalidade}
+           value={nacionalidade}/>
           <Text style={styles.erro}>{erroNacionalidade}</Text>
 
-          <TextInput 
-            placeholder="Categoria CNH" 
-            style={styles.input} 
-            onChangeText={CategoriaCnh} 
-          />
+          <TextInput
+           placeholder="Categoria CNH"
+           style={styles.input}
+           onChangeText={CategoriaCnh}
+           value={cnhCategoria}/>
           <Text style={styles.erro}>{erroCnhCategoria}</Text>
 
 
-          <TextInput 
-          placeholder="Data de Emissão CNH"
+          <TextInput
+           placeholder="Data de Emissão CNH"
            keyboardType="numeric"
-          style={styles.input} onChangeText={setCnhEmissao} 
-          />
+           style={styles.input}
+           onChangeText={setCnhEmissao}
+           value={cnhEmissao}/>
           <Text style={styles.erro}>{erroCnhEmissao}</Text>
 
-          <TextInput 
-          placeholder="Data de Validade CNH"
-           keyboardType="numeric" style={styles.input}
-            onChangeText={setCnhValidade} 
-            />
+          <TextInput
+           placeholder="Data de Validade CNH"
+           keyboardType="numeric"
+           style={styles.input}
+           onChangeText={setCnhValidade}
+           value={cnhValidade}/>
           <Text style={styles.erro}>{erroCnhValidade}</Text>
 
-          <TextInput 
-          placeholder="RG" 
-          keyboardType="numeric"
+          <TextInput
+           placeholder="RG"
+           keyboardType="numeric"
            style={styles.input}
-            onChangeText={setRg} />
+           onChangeText={setRg}
+           value={rg}/>
           <Text style={styles.erro}>{erroRg}</Text>
 
-          <TextInput 
-          placeholder="CPF"
-           keyboardType="numeric" 
-           style={styles.input} onChangeText={setCpf} />
+          <TextInput
+           placeholder="CPF"
+           keyboardType="numeric"
+           style={styles.input}
+           onChangeText={setCpf}
+           value={cpf}/>
           <Text style={styles.erro}>{erroCpf}</Text>
 
           <Button title="Próximos passos" onPress={continuarCadastro} />
@@ -280,18 +282,30 @@ function CategoriaCnh(texto) {
         <>
           <Text style={styles.titulo}>Entrar na sua conta</Text>
 
-          <TextInput placeholder="Email" keyboardType="email-address" style={styles.input} onChangeText={setEmailLogin} />
+          <TextInput
+           placeholder="Email"
+           keyboardType="email-address"
+           style={styles.input}
+           onChangeText={setEmailLogin}
+           value={emailLogin}
+          />
           <Text style={styles.erro}>{erroEmailLogin}</Text>
 
-          <TextInput placeholder="Senha" secureTextEntry style={styles.input} onChangeText={setSenhaLogin} />
+          <TextInput
+           placeholder="Senha"
+           secureTextEntry
+           style={styles.input}
+           onChangeText={setSenhaLogin}
+           value={senhaLogin}
+          />
           <Text style={styles.erro}>{erroSenhaLogin}</Text>
 
-          <TextInput 
-            placeholder="Número de Registro" 
-            keyboardType="numeric" 
+          <TextInput
+            placeholder="Número de Registro"
+            keyboardType="numeric"
             style={styles.input}
             onChangeText={NumeroRegistro}
-            value={numeroRegistro} 
+            value={numeroRegistro}
           />
 
           <Button title="Entrar" onPress={login} />
@@ -310,7 +324,7 @@ function CategoriaCnh(texto) {
 }
 
 const styles = StyleSheet.create({
-  container: { flexGrow: 1, alignItems: "center", padding: 20 },
+  container: { height: altura, flexGrow: 1, alignItems: "center", padding: 20 },
   titulo: { fontSize: 20, fontWeight: "bold", marginBottom: 15 },
   input: { width: "80%", padding: 10, borderWidth: 1, marginVertical: 5, borderRadius: 5 },
   link: { color: "blue", marginTop: 10 },
