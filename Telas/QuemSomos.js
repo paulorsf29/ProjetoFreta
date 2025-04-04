6d59d894ad554bd03a7ddc07e725699ab19791e2
ElprogramadorxX <mdarruda@gmail.com>
2025-04-04 20:16:38 -0300

    Adicionando codigo atualizado

--- Telas/QuemSomos.js
+++ Telas/QuemSomos.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function MenuCliente({navigation}) {
+export default function MenuCliente({ navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.textContainer}>
-        <Text style={styles.text}>O que é Freta</Text>
-        <Text style={styles.text}>Freta é uma ferramenta de gestão de fretes com confiabilidade e segurança</Text>
+        <Text style={styles.title}>O que é Freta</Text>
+        <Text style={styles.text}>
+        A Freta é uma plataforma de gestão de fretes que conecta clientes e caminhoneiros de forma prática e segura. Com tecnologia intuitiva e foco na transparência, agiliza negociações, reduz custos e oferece controle total sobre o transporte.
+        </Text>
       </View>
     </View>
   );
@@ -14,14 +16,26 @@ export default function MenuCliente({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1a1a1a',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    textAlign: 'center'
+    padding: 20,
   },
   textContainer: {
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    color: '#FFD700',
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: '#f2f2f2',
+    textAlign: 'justify',
+    lineHeight: 26,
+  },
 });
